9362bb54324f7f7ff0ad51902fb3afc02b9b1e43
BhathikaGimhan <bgmaduragoda@gmail.com>
2023-10-23 17:43:00 +0530

    upload

--- src/Tools/Status.jsx
+++ src/Tools/Status.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Status = () => {
   return (
     <div className='status-view'>
-        <p>hello</p>
+        <div className="status-body">Active</div>
+        <div className="active-btn">
+            <span class="relative flex h-3 w-3">
+                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                <span class="relative flex justify-center items-center m-auto animate-pulse rounded-full h-2 w-2 bg-sky-500"></span>
+            </span>
+        </div>
     </div>
   )
 }
