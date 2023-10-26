2e75fb6690e6930a5d2656f7d505b1238df3e17d
BhathikaGimhan <bgmaduragoda@gmail.com>
2023-10-27 01:11:27 +0530

    update

--- src/Tools/user/SignIn.jsx
+++ src/Tools/user/SignIn.jsx
@@ -1,31 +1,11 @@
 import React from 'react'
-import { Button, Form } from 'react-bootstrap'
 
-const SignIn = ({ handleShowSignUp }) => {
+const SignIn = () => {
   return (
     <>
-      <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-    <hr />
-    <p>Login with Google: <div className="login-icon" onClick={handleShowSignUp}></div><code>(Recommended)</code></p>
+      <button type="button" class="login-with-google-btn w-full" >
+        Sign in with Google
+      </button>
     </>
 
   )
--- src/Tools/user/SignUp.jsx
+++ src/Tools/user/SignUp.jsx
@@ -1,12 +1,29 @@
 // import { Modal } from 'bootstrap'
+import { Button, Form } from 'react-bootstrap'
 import React from 'react'
 
-const SignUp = () => {
+const SignUp = ({ handleShowSignUp }) => {
   return (
     <div>
-      <button type="button" class="login-with-google-btn w-full" >
-        Sign in with Google
-      </button>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Your Name <code>*</code></Form.Label>
+        <Form.Control type="text" placeholder="Enter yor name" />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Select your positon <code>*</code></Form.Label>
+        <Form.Select aria-label="Default select example">
+        <option>Open this select menu</option>
+        <option value="1">Student</option>
+        <option value="2">Lectur</option>
+        <option value="3">Other</option>
+        </Form.Select>
+      </Form.Group>
+      <br />
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
     </div>
   )
 }
