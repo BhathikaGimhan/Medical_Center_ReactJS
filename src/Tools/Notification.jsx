f322986d11e06840bbcea953e1044cbea4914341
BhathikaGimhan <bgmaduragoda@gmail.com>
2023-10-29 16:58:13 +0530

    crate botification

--- src/Tools/Notification.jsx
+++ src/Tools/Notification.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Toast } from 'react-bootstrap'
 
 const Notification = () => {
   return (
-    <div>Notification</div>
+    <div>
+        <Toast>
+          <Toast.Header>
+            <img
+              src="holder.js/20x20?text=%20"
+              className="rounded me-2"
+              alt=""
+            />
+            <strong className="me-auto">Bootstrap</strong>
+            <small className="text-muted">2 seconds ago</small>
+          </Toast.Header>
+          <Toast.Body>Heads up, toasts will stack automatically</Toast.Body>
+        </Toast>
+    </div>
   )
 }
 
