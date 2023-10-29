0292625858ce986babf5d1c934f73bfbd033418c
BhathikaGimhan <bgmaduragoda@gmail.com>
2023-10-29 14:31:18 +0530

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,6 @@ import { getFirestore } from "@firebase/firestore"
 import { getAuth, GoogleAuthProvider } from "@firebase/auth";
 
 const firebaseConfig = {
-    // apiKey: "AIzaSyCn2L_mOhKRsJY1QYCx_iZymj_uUm2yTZs",
-    // authDomain: "hopeful-nova-399405.firebaseapp.com",
-    // databaseURL: "https://hopeful-nova-399405-default-rtdb.asia-southeast1.firebasedatabase.app",
-    // projectId: "hopeful-nova-399405",
-    // storageBucket: "hopeful-nova-399405.appspot.com",
-    // messagingSenderId: "425820017237",
-    // appId: "1:425820017237:web:5572d84a18593927ddfadc"
-
     apiKey: "AIzaSyCVPvnxd6qFTwcaFuf3-GXhUE7p4c6lcr8",
     authDomain: "helth-care-app-trincomalee.firebaseapp.com",
     projectId: "helth-care-app-trincomalee",
