93ac77e8f3e0dbd198b1f3c58fbfdab5092e9ef4
BhathikaGimhan <bgmaduragoda@gmail.com>
2023-12-04 00:26:13 +0530

    Add react-firebase-hooks package and implement status change functionality

--- src/Tools/Status.jsx
+++ src/Tools/Status.jsx
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { firestore } from '../firebase'; // import your firebase config
+import { getDocs, collection, doc, updateDoc } from 'firebase/firestore';
 
 const Status = () => {
-  let status = 1;
+  const [status, setStatus] = useState(null);
+  const user = localStorage.getItem('userRole');
+  console.log(user);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const querySnapshot = await getDocs(collection(firestore, 'status'));
+    
+      querySnapshot.forEach(doc => {
+        const StatusData = doc.data();
+        console.log(StatusData.status);
+        setStatus(StatusData.status);
+      });
+    };
+    
+    fetchData();
+  }, []);
+
+  const handleStatusChange = async () => {
+    // Check if the user has admin role before updating the status
+    if (user === 'admin') {
+      // Toggle the status (1 to 0 or 0 to 1)
+      const newStatus = status === 1 ? 0 : 1;
+
+      // Update the status in firestore
+      const statusRef = doc(firestore, 'status', 'MFYzJK5Ql5IUKT9TKVZI');
+      await updateDoc(statusRef, { status: newStatus });
+
+      // Update the local state
+      setStatus(newStatus);
+    }
+  };
+
+  if (status === null) return null;
+
   return (
     <>
-      <div className={`status-view ${status === 1 ? 'active' : 'deactive'} bg-white`}>
+      
+
+      {/* <div className={`status-view ${status === 1 ? 'active' : 'deactive'} bg-white`}>
         <div className={`status-body ${status === 1 ? 'text-blue-500' : 'text-red-500'}`}>
           {status === 1 ? 'Active' : 'Deactive'}
         </div>
+      </div> */}
+
+
+
+      <div className={`status-view ${status === 1 ? 'active' : 'deactive'} bg-white`}>
+      {user === 'admin' ? (
+        <div onClick={handleStatusChange} className={`status-body cursor-pointer ${status === 1 ? 'text-blue-500' : 'text-red-500'}`}>
+          Change
+        </div>
+      ):(
+        <div className={`status-body ${status === 1 ? 'text-blue-500' : 'text-red-500'}`}>
+          {status === 1 ? 'Active' : 'Deactive'}
+        </div>
+      )}
+        
         <div className="active-btn absolute">
           <span className="relative z-50 flex h-3 w-3">
             <span className={`animate-ping absolute inline-flex h-full w-full rounded-full ${status === 1 ? 'bg-sky-400' : 'bg-rose-400'} opacity-75`}></span>
@@ -16,6 +69,7 @@ const Status = () => {
         </div>
       </div>
     </>
+
   );
 }
 
